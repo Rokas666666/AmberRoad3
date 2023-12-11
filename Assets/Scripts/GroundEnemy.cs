4bc0d693ad94e46fbb7e300371aaa67832839e13
Rokas666666 <60842961+Rokas666666@users.noreply.github.com>
2023-12-11 13:21:40 +0200

    more fixes

--- Assets/Scripts/GroundEnemy.cs
+++ Assets/Scripts/GroundEnemy.cs
@@ -39,12 +39,18 @@ public class GroundEnemy : MonoBehaviour
 
     private float HitDelay = 1.0f;
 
+    //[SerializeField]
+    //public Tower tower;
+
     void Start()
     {
         me = GetComponent<Enemy>();
         target = GameObject.FindWithTag("Tower");
         SetTargetHPS();
         StartCoroutine(HitTower());
+        //tower = GetComponent<Tower>();
+        //tower.addResources(1000);
+        InvokeRepeating("Shoot", 1, FireRate);
         agent = GetComponent<NavMeshAgent>();
         agent.speed = initialSpeed;
     }
@@ -191,5 +197,69 @@ public class GroundEnemy : MonoBehaviour
             yield return new WaitForSeconds(HitDelay);
         }
         yield return null;
+    }
+
+
+    [SerializeField]
+    private List<GameObject> Targets = new List<GameObject>();
+    [SerializeField]
+    private string EnemyTag;
+    [SerializeField]
+    float FireRate = 1.0f;
+    [SerializeField]
+    private Projectile projectile;
+    [SerializeField]
+    private Transform shootingPoint;
+    [SerializeField]
+    private bool ShootEnabled = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(EnemyTag))
+        {
+            Targets.Add(other.gameObject);
+            Debug.Log("Add method Enemies: " + Targets.Count);
+
+            // Assuming the enemy script is on the same GameObject
+            Enemy enemyScript = other.GetComponent<Enemy>();
+
+            if (enemyScript != null)
+            {
+                // Subscribe to the OnEnemyKilled event
+                enemyScript.OnEnemyKilled += HandleEnemyKilled;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)//dis no work
+    {
+        if (other.CompareTag(EnemyTag))
+        {
+            Targets.Remove(other.gameObject);
+            Debug.Log("Remove method Enemies: " + Targets.Count);
+        }
+    }
+
+    private void HandleEnemyKilled(Enemy enemy)
+    {
+        // This method will be called every time an enemy is killed
+        // You can put your logic here
+        Targets.Remove(enemy.gameObject);
+    }
+
+    private void Shoot()
+    {
+        // Check and clean up the Targets list periodically
+        CleanUpTargetsList();
+
+        if (Targets.Count <= 0 || !ShootEnabled) return;
+        projectile.SetTarget(Targets[0]);
+        Instantiate(projectile, shootingPoint.position, projectile.transform.rotation);
+    }
+
+    private void CleanUpTargetsList()
+    {
+        // Remove null entries from the Targets list
+        Targets.RemoveAll(target => target == null);
     }
 }
